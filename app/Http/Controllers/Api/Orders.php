ec4d60c5001dc925ab07ecb14a048e8d945cf75b
fneco <izumi.f.tech@gmail.com>
2023-12-21 18:26:34 +0900

    feat: list 2‒8

--- app/Http/Controllers/Api/Orders.php
+++ app/Http/Controllers/Api/Orders.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateOrderRequest;
 use App\Util\OpenAPI\Client\Validate;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -10,14 +11,14 @@ use OpenAPI\Client\Model\CreateOrderSchema;
 
 class Orders extends Controller
 {
-    public function store(Request $request): Response
+    public function store(CreateOrderRequest $request): Response
     {
         Validate::with($request->all(), CreateOrderSchema::class);
 
         return $this->defaultResponse();
     }
 
-    public function update(Request $request): Response
+    public function update(CreateOrderRequest $request): Response
     {
 
         Validate::with($request->all(), CreateOrderSchema::class);
--- app/Http/Requests/CreateOrderRequest.php
+++ app/Http/Requests/CreateOrderRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Http\Controllers\Api;
 
-class OrdersRequest extends FormRequest
+class CreateOrderRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,7 +23,7 @@ class OrdersRequest extends FormRequest
     public function rules(): array
     {
         return [
-                'order' => ['required', new Api\OrdersRules()]
-            ];
+            'order.*.quantity' => ['required']
+        ];
     }
 }
